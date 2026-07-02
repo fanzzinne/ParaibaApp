1dd18a5ad4d1c110a7fb2de644caad3ea4acbb97
fanzzinne <fanzzinne@gmail.com>
2026-07-02 19:27:59 -0300

    commit

--- sw.js
+++ sw.js
@@ -1,15 +1,16 @@
-const CACHE_NAME = 'catalogo-v1';
+const CACHE_NAME = 'paraiba-v2';
+const APP_PREFIX = '/ParaibaApp';
+
 const ASSETS = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './app/src/main/res/drawable/logo.png',
+  APP_PREFIX + '/',
+  APP_PREFIX + '/index.html',
+  APP_PREFIX + '/app.js',
+  APP_PREFIX + '/manifest.json',
+  APP_PREFIX + '/logo.png',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
-// Instalação do Service Worker e cache de arquivos estáticos
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -18,7 +19,6 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Intercepta as requisições para servir o cache quando offline
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
